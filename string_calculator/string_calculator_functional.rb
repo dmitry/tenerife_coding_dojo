adc1092a9933b44ec69bed4c18e1b08146d9258d
Dmitry Polushkin <dmitry.polushkin@gmail.com>
2014-01-13 01:19:58 +0000

    one more functional example passed for string calculator

--- string_calculator/string_calculator_functional.rb
+++ string_calculator/string_calculator_functional.rb
@@ -1,15 +1,33 @@
 class StringCalculatorFunctional
+  DEFAULT_SPLITTER = /[;,\n]/
+
   def self.add(string)
     return 0 if empty?(string)
-    return convert_and_sum(split_by_comma(string))
+    return convert_and_sum(split(string))
   end
 
   def self.empty?(string)
     string.empty?
   end
 
-  def self.split_by_comma(string)
-    string.split(retrieve_delimeter(string))
+  def self.split(string)
+    split_by_delimiter(cleanup_from_prefix_delimiter(string), retrieve_delimeter(string))
+  end
+
+  def self.split_by_delimiter(string, delimiter)
+    string.split(delimiter)
+  end
+
+  def self.cleanup_from_prefix_delimiter(string)
+    remove_from_delimiter(match_prefix_delimiter(string), string)
+  end
+
+  def self.remove_from_delimiter(match, string)
+    if match
+      string.gsub(match[0], '')
+    else
+      string
+    end
   end
 
   def self.convert_and_sum(strings)
@@ -43,7 +61,7 @@ class StringCalculatorFunctional
     if matches
       matches[1]
     else
-      /[;,\n]/
+      DEFAULT_SPLITTER
     end
   end
 end
